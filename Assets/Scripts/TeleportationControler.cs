8d01e49a99d5fab1d1adfa404def1282075657f4
Maxime Boucher <mboucher@apollocode.ca>
2023-04-19 12:36:52 -0400

    Placement des arme

---
+++ Assets/Scripts/TeleportationControler.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class TeleportationControler : MonoBehaviour
+{
+    public bool DisableRightTeleportation { get; set ;} = false;
+    //disable teleportation
+    public void DisableTeleportation()
+    {
+        
+    }
+}
