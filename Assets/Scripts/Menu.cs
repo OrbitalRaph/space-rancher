ecf1572d3eebfeb73f0c26fe3cb68019812d1c8f
Maxime Boucher <mboucher@apollocode.ca>
2023-05-07 15:45:17 -0400

    ajout menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Didacticiel()
+    {
+        SceneManager.LoadScene("Didacticiel");
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Option()
+    {
+        SceneManager.LoadScene("Option");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }   
+}
